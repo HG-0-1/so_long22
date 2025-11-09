921b97f96ede32b074f02c9dd4cecde5c8356ed6
HG-0-1 <alfayezh844@gmail.com>
2025-11-09 18:49:06 +0300

    Update move_my_man.c

--- move_my_man.c
+++ move_my_man.c
@@ -31,24 +31,47 @@ void find_my_man(t_game *game)
     
 }
 
-
+void draw_player(t_game *game)
+{
+    mlx_put_image_to_window(game -> mlx, game -> win,
+game -> player, game -> player_x *64, game -> player_y * 64);
+}
 void update_game(t_game *game)
 {
     mlx_clear_window(game -> mlx ,game -> win);
-    dr
+    img_in_map(game);
+    draw_player(game);
 }
-void move_to_up(t_game *game)
+void move_up(t_game *game)
 {
-    if (game -> map[game -> player - 1][game -> player_x] != '1')
+    if (game -> map[game -> player_y - 1][game -> player_x] != '1')
     {
         game -> player_y--;
         game -> moves++;
         update_game(game);
     }
 }
-void move_to_down(t_game *game)
+void move_down(t_game *game)
 {
-    if (game -> map[game -> player + 1][game -> player_x] != '1')
+    if (game -> map[game -> player_y + 1][game -> player_x] != '1')
+    {
+        game -> player_y++;
+        game -> moves++;
+        update_game(game);
+    }
+}
+void move_left(t_game game)
+{
+     if (game -> map[game -> player_y - 1][game -> player_x -1] != '1')
+     {
+        game -> player_y--;
+        game -> moves++;
+        update_game(game);
+     }
+}
+void move_right(t_game *game)
+{
+    if (game -> map[game -> player_y + 1][game -> player_x + 1] != '1')
     {
         game -> player_y++;
         game -> moves++;
